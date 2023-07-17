39372b06540e98785ccac324dd430aed9ae363b7
victoriaborisovaa <viktoria.vas.marinova@gmail.com>
2023-07-17 12:34:16 +0300

    Initial commit

---
+++ src/main/java/Computer.java
@@ -0,0 +1,20 @@
+public class Computer {
+    int year;
+    double price;
+    String operationSystem;
+
+    public Computer(int year, double price, String operationSystem) {
+        this.year = year;
+        this.price = price;
+        this.operationSystem = operationSystem;
+    }
+   public int comparePrice(Computer compare){
+        if(this.price > compare.price) {
+            return -1;
+        } else if (this.price < compare.price){
+            return 1;
+        } else {
+            return 0;
+        }
+   }
+}
---
+++ src/main/java/ComputerPrices.java
@@ -0,0 +1,16 @@
+public class ComputerPrices {
+    public static void main(String[] args) {
+        Computer Windows = new Computer (2019, 3000.0,"Windows");
+        Computer Mac = new Computer(2023, 5000.8, "MacOs");
+
+        int priceComparison = Windows.comparePrice(Mac);
+        if (priceComparison == -1){
+            System.out.println("The price of Windows is higher than Mac.");
+        } else if (priceComparison == 1) {
+            System.out.println("The price of Mac is higher than Windows.");
+        } else {
+            System.out.println("The prices of Windows and Mac are the same.");
+
+    }
+}
+}
